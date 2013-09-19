d9172af0bcb002fc36ac01c903d1fbdb383f0b78
starscrazy <starscrazy@c8c01e27-c2b2-4c14-9fc5-b7b90e2c6539>
2013-09-19 20:27:02 +0000

    Mit dem trunk gemerged (der Rest)
    git-svn-id: svn://gwron.kicks-ass.net/TVTower@296 c8c01e27-c2b2-4c14-9fc5-b7b90e2c6539

--- branches/quotesAndTrendings/res/ai/TaskAdAgency.lua
+++ branches/quotesAndTrendings/res/ai/TaskAdAgency.lua
@@ -1,5 +1,5 @@
 -- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-TaskAdAgency = AITask:new{	
+TaskAdAgency = AITask:new{
 	TargetRoom = TVT.ROOM_ADAGENCY;
 	SpotsInAgency = nil;
 	BasePriority = 8;
@@ -19,33 +19,33 @@ function TaskAdAgency:Activate()
 	-- Was getan werden soll:
 	self.CheckSpots = JobCheckSpots:new()
 	self.CheckSpots.AdAgencyTask = self
-	
+
 	self.AppraiseSpots = AppraiseSpots:new()
 	self.AppraiseSpots.AdAgencyTask = self
-	
+
 	self.SignRequisitedContracts = SignRequisitedContracts:new()
-	self.SignRequisitedContracts.AdAgencyTask = self		
-	
+	self.SignRequisitedContracts.AdAgencyTask = self
+
 	self.SignContracts = SignContracts:new()
-	self.SignContracts.AdAgencyTask = self	
-	
+	self.SignContracts.AdAgencyTask = self
+
 	self.SpotsInAgency = {}
 end
 
 function TaskAdAgency:GetNextJobInTargetRoom()
-	if (MY.ProgrammeCollection.GetContractCount() >= 8) then
+	if (MY.ProgrammeCollection.GetAdContractCount() >= 8) then
 		self:SetDone()
 		return nil
 	elseif (self.CheckSpots.Status ~= JOB_STATUS_DONE) then
 		return self.CheckSpots
 	elseif (self.AppraiseSpots.Status ~= JOB_STATUS_DONE) then
 		return self.AppraiseSpots
-	elseif (self.SignRequisitedContracts.Status ~= JOB_STATUS_DONE) then	
-		return self.SignRequisitedContracts		
-	elseif (self.SignContracts.Status ~= JOB_STATUS_DONE) then	
+	elseif (self.SignRequisitedContracts.Status ~= JOB_STATUS_DONE) then
+		return self.SignRequisitedContracts
+	elseif (self.SignContracts.Status ~= JOB_STATUS_DONE) then
 		return self.SignContracts
 	end
-	
+
 	self:SetDone()
 end
 -- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
@@ -78,17 +78,17 @@ function JobCheckSpots:CheckSpot()
 	if ((spotId == -2) or (spotId == -8)) then
 		self.Status = JOB_STATUS_DONE
 		return
-	end	
+	end
 
 	local spot = TVT.GetContract(spotId)
-	
+
 	if (spot.IsAvailableToSign() == 1) then
 		--debugMsg("Signable")
 		local player = _G["globalPlayer"]
 		self.AdAgencyTask.SpotsInAgency[self.CurrentSpotIndex] = spot
-		player.Stats:AddSpot(spot)	
+		player.Stats:AddSpot(spot)
 	end
-	
+
 	self.CurrentSpotIndex = self.CurrentSpotIndex + 1
 end
 -- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
@@ -116,7 +116,7 @@ function AppraiseSpots:AppraiseCurrentSpot()
 		self:AppraiseSpot(spot)
 		self.CurrentSpotIndex = self.CurrentSpotIndex + 1
 	else
-		self.Status = JOB_STATUS_DONE		
+		self.Status = JOB_STATUS_DONE
 	end
 end
 
@@ -127,23 +127,23 @@ function AppraiseSpots:AppraiseSpot(spot)
 	--debugMsg("===================")
 	local player = _G["globalPlayer"]
 	local stats = player.Stats
-	local score = -1	
-	
+	local score = -1
+
 	if (spot.GetMinAudience() > stats.Audience.MaxValue) then
 		--spot.Appraisal = -2
 		--debugMsg("zu viele Zuschauer verlangt! " .. spot.Audience .. " / " .. stats.Audience.MaxValue)
 		return
 	end
-	
+
 	--debugMsg("spot.SpotProfit: " .. spot.SpotProfit .. " ; spot.SpotToSend: " .. spot.SpotToSend)
 	local profitPerSpot = spot.GetProfit() / spot.GetSpotCount()
 	--debugMsg("profitPerSpot: " .. profitPerSpot .. " ; stats.SpotProfitPerSpotAcceptable.AverageValue: " .. stats.SpotProfitPerSpotAcceptable.AverageValue)
-	local financePower = profitPerSpot / stats.SpotProfitPerSpotAcceptable.AverageValue	
+	local financePower = profitPerSpot / stats.SpotProfitPerSpotAcceptable.AverageValue
 	--debugMsg("financePower1: " .. financePower)
 	financePower = CutFactor(financePower, 0.2, 2)
 	--debugMsg("financePower: " .. financePower)
 
-	-- 2 = Locker zu schaffen / 0.3 schwierig zu schaffen	
+	-- 2 = Locker zu schaffen / 0.3 schwierig zu schaffen
 	local audienceFactor = stats.Audience.AverageValue / spot.GetMinAudience()
 	audienceFactor = CutFactor(audienceFactor, 0.3, 2)
 	--debugMsg("audienceFactor: " .. audienceFactor .. " ; stats.Audience.AverageValue: " .. stats.Audience.AverageValue .. " ; spot.Audience:" .. spot.Audience)
@@ -154,19 +154,19 @@ function AppraiseSpots:AppraiseSpot(spot)
 	riskFactor = riskFactor * audienceFactor
 	riskFactor = CutFactor(riskFactor, 0.2, 2)
 	--debugMsg("riskFactor: " .. riskFactor .. " ; SpotPenalty: " .. stats.SpotPenalty.AverageValue .. " ; SpotPenalty:" .. spot.SpotPenalty)
-		
+
 	-- 2 leicht zu packen / 0.3 hoher Druck
 	local pressureFactor = spot.GetDaysToFinish() / spot.GetSpotCount()
 	pressureFactor = CutFactor(pressureFactor, 0.2, 2)
 	--debugMsg("pressureFactor: " .. pressureFactor .. " ; SpotMaxDays: " .. spot.SpotMaxDays .. " ; SpotToSend:" .. spot.SpotToSend)
-		
+
 	spot.SetAttractiveness(audienceFactor * riskFactor * pressureFactor)
 	--debugMsg("Spot-Attractiveness: ===== " .. spot.GetAttractiveness() .. " ===== ; financePower: " .. financePower .. " ; audienceFactor: " .. audienceFactor .. " ; riskFactor: " .. riskFactor .. " ; pressureFactor: " .. pressureFactor)
-	
+
 	--debugMsg("===================")
-	
+
 	--financeBase
-	
+
 	-- Je höher der Gewinn desto besser
 	-- Je höher die Strafe desto schlechter
 	-- Je geringer die benötigten Zuschauer desto besser
@@ -185,40 +185,40 @@ SignRequisitedContracts = AIJob:new{
 function SignRequisitedContracts:Prepare(pParams)
 	debugMsg("Unterschreibe benötigte Werbeverträge")
 	self.CurrentSpotIndex = 0
-	
+
 	self.Player = _G["globalPlayer"]
-	self.SpotRequisitions = self.Player:GetRequisitionsByTaskId(_G["TASK_ADAGENCY"])	
+	self.SpotRequisitions = self.Player:GetRequisitionsByTaskId(_G["TASK_ADAGENCY"])
 end
 
-function SignRequisitedContracts:Tick()	
+function SignRequisitedContracts:Tick()
 	--debugMsg("SignRequisitedContracts")
-	
+
 	--Sortieren
 	local sortMethod = function(a, b)
 		return a.GetAttractiveness() > b.GetAttractiveness()
-	end	
-	table.sort(self.AdAgencyTask.SpotsInAgency, sortMethod)	
-	
+	end
+	table.sort(self.AdAgencyTask.SpotsInAgency, sortMethod)
+
 	for k,requisition in pairs(self.SpotRequisitions) do
 		local neededSpotCount = requisition.Count
-		
+
 		local guessedAudience = AITools:GuessedAudienceForLevel(requisition.Level)
 		local minGuessedAudience = (guessedAudience * 0.8)
-				
+
 		local signedContracts = self:SignMatchingContracts(requisition, guessedAudience, self:GetMinGuessedAudience(guessedAudience, 0.8))
-		if (signedContracts == 0) then		
+		if (signedContracts == 0) then
 			signedContracts = self:SignMatchingContracts(requisition, guessedAudience, self:GetMinGuessedAudience(guessedAudience, 0.6))
 			if (signedContracts == 0) then
-				guessedAudience = guessedAudience + 5000 -- Die 5000 sind einfach ein Erfahrungswert, denn es gibt kaum kleinere Werbeverträge... die Sinnhaftigkeit sollte nochmal geprüft werden								
-				signedContracts = self:SignMatchingContracts(requisition, guessedAudience, self:GetMinGuessedAudience(guessedAudience, 0.6))					
+				guessedAudience = guessedAudience + 5000 -- Die 5000 sind einfach ein Erfahrungswert, denn es gibt kaum kleinere Werbeverträge... die Sinnhaftigkeit sollte nochmal geprüft werden
+				signedContracts = self:SignMatchingContracts(requisition, guessedAudience, self:GetMinGuessedAudience(guessedAudience, 0.6))
 				if (signedContracts == 0) then
 					guessedAudience = guessedAudience + 5000 -- Die 5000 sind einfach ein Erfahrungswert, denn es gibt kaum kleinere Werbeverträge... die Sinnhaftigkeit sollte nochmal geprüft werden
-					signedContracts = self:SignMatchingContracts(requisition, guessedAudience, self:GetMinGuessedAudience(guessedAudience, 0.6))									
+					signedContracts = self:SignMatchingContracts(requisition, guessedAudience, self:GetMinGuessedAudience(guessedAudience, 0.6))
 				end
 			end
 		end
-	end	
-	
+	end
+
 	self.Status = JOB_STATUS_DONE
 end
 
@@ -233,34 +233,34 @@ end
 function SignRequisitedContracts:SignMatchingContracts(requisition, guessedAudience, minguessedAudience)
 	local signed = 0
 	local buyedContracts = {}
-	
+
 	for key, value in pairs(self.AdAgencyTask.SpotsInAgency) do
-		if MY.ProgrammeCollection.GetContractCount() >= 8 then break end
-	
+		if MY.ProgrammeCollection.GetAdContractCount() >= 8 then break end
+
 		local minAudience = value.GetMinAudience()
-	
+
 		if ((minAudience < guessedAudience) and (minAudience > minguessedAudience)) then
 			--Passender Spot... also kaufen
-			debugMsg("Schließe Werbevertrag: " .. value.contractBase.title .. " (" .. value.GetID() .. ") weil benötigt. Level: " .. requisition.Level)
+			debugMsg("Schließe Werbevertrag: " .. value.GetTitle() .. " (" .. value.GetID() .. ") weil benötigt. Level: " .. requisition.Level)
 			TVT.sa_doBuySpot(value.GetID())
 			requisition:UseThisContract(value)
-			table.insert(buyedContracts, value)			
-			signed = signed + 1			
+			table.insert(buyedContracts, value)
+			signed = signed + 1
 			--neededSpotCount = neededSpotCount - value.GetSpotCount()
 		end
-		
+
 		--if (neededSpotCount <= 0) then
 		--	self.Player:RemoveRequisition(requisition)
 		--else
 		--	requisition.Count = neededSpotCount
-		--end		
+		--end
 	end
-	
+
 	if (table.count(buyedContracts) > 0) then
 		debugMsg("Entferne " .. table.count(buyedContracts) .. " abgeschlossene Werbeverträge aus der Shop-Liste.")
 		table.removeCollection(self.AdAgencyTask.SpotsInAgency, buyedContracts)
 	end
-	
+
 	return signed
 end
 -- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
@@ -278,39 +278,39 @@ function SignContracts:Prepare(pParams)
 	self.CurrentSpotIndex = 0
 end
 
-function SignContracts:Tick()	
+function SignContracts:Tick()
 	--debugMsg("SignContracts")
-	
+
 	--Sortieren
 	local sortMethod = function(a, b)
 		return a.GetAttractiveness() > b.GetAttractiveness()
-	end	
+	end
 	table.sort(self.AdAgencyTask.SpotsInAgency, sortMethod)
-	
+
 	local openSpots = self:GetCommonRequisition()
 	--debugMsg("openSpots: " .. openSpots)
 	if (openSpots > 0) then
 		for key, value in pairs(self.AdAgencyTask.SpotsInAgency) do
-			if MY.ProgrammeCollection.GetContractCount() >= 8 then break end
+			if MY.ProgrammeCollection.GetAdContractCount() >= 8 then break end
 			if (openSpots > 0) then
 				openSpots = openSpots - value.GetSpotCount()
-				debugMsg("Schließe Werbevertrag: " .. value.contractBase.title .. " (" .. value.GetID() .. ")")
-				TVT.sa_doBuySpot(value.GetID())				
+				debugMsg("Schließe Werbevertrag: " .. value.GetTitle() .. " (" .. value.GetID() .. ")")
+				TVT.sa_doBuySpot(value.GetID())
 			end
-		end	
+		end
 	end
-	
+
 	self.Status = JOB_STATUS_DONE
 end
 
 function SignContracts:GetCommonRequisition()
 	local unsendedSpots = 0
 
-	for i = 0, MY.ProgrammeCollection.GetContractCount() - 1 do
-		local contract = MY.ProgrammeCollection.GetContractFromList(i)
-		local count = MY.ProgrammePlan.GetContractBroadcastCount(contract.id, 1, 0)
-		--debugMsg("GetMatchingSpotList: " .. contract.title .. " - " .. count)			
-		
+	for i = 0, MY.ProgrammeCollection.GetAdContractCount() - 1 do
+		local contract = MY.ProgrammeCollection.GetAdContractFromList(i)
+		local count = MY.ProgrammePlan.GetAdContractBroadcastCount(contract.id, 1, 0)
+		--debugMsg("GetMatchingSpotList: " .. contract.title .. " - " .. count)
+
 		if (count < contract.GetSpotCount()) then
 			unsendedSpots = unsendedSpots + (contract.GetSpotCount() - count)
 		end
--- branches/quotesAndTrendings/res/ai/TaskNewsAgency.lua
+++ branches/quotesAndTrendings/res/ai/TaskNewsAgency.lua
@@ -15,7 +15,7 @@ function TaskNewsAgency:Activate()
 	-- Was getan werden soll:
 	self.NewsAgencyAbonnementsJob = JobNewsAgencyAbonnements:new()
 	self.NewsAgencyAbonnementsJob.Task = self
-	
+
 	self.NewsAgencyJob = JobNewsAgency:new()
 	self.NewsAgencyJob.Task = self
 end
@@ -27,15 +27,15 @@ function TaskNewsAgency:GetNextJobInTargetRoom()
 	if (self.NewsAgencyJob.Status ~= JOB_STATUS_DONE) then
 		return self.NewsAgencyJob
 	end
-	
+
 	self:SetDone()
 end
 
-function TaskNewsAgency:BudgetSetup()	
+function TaskNewsAgency:BudgetSetup()
 	local tempAbonnementBudget = self.BudgetWholeDay * 0.55
-	self.AbonnementBudget = (tempAbonnementBudget - (tempAbonnementBudget % 10000))		
+	self.AbonnementBudget = (tempAbonnementBudget - (tempAbonnementBudget % 10000))
 	self.CurrentBudget = self.CurrentBudget - self.AbonnementBudget
-	--debugMsg("BudgetSetup: AbonnementBudget: " .. self.AbonnementBudget .. "   - CurrentBudget: " .. self.CurrentBudget)	
+	--debugMsg("BudgetSetup: AbonnementBudget: " .. self.AbonnementBudget .. "   - CurrentBudget: " .. self.CurrentBudget)
 end
 -- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
@@ -45,18 +45,18 @@ JobNewsAgencyAbonnements = AIJob:new{
 }
 
 function JobNewsAgencyAbonnements:Prepare(pParams)
-	debugMsg("Prüfe/Schließe Nachrichtenabonnements")	
+	debugMsg("Prüfe/Schließe Nachrichtenabonnements")
 end
 
 function JobNewsAgencyAbonnements:Tick()
 	local abonnementCount = self.Task.AbonnementBudget / 10000
-	
+
 	MY.SetNewsAbonnement(TVT.NEWS_GENRE_POLITICS, self:GetAbonnementLevel(abonnementCount, 1))
 	MY.SetNewsAbonnement(TVT.NEWS_GENRE_SHOWBIZ, self:GetAbonnementLevel(abonnementCount, 2))
 	MY.SetNewsAbonnement(TVT.NEWS_GENRE_SPORT, self:GetAbonnementLevel(abonnementCount, 3))
 	MY.SetNewsAbonnement(TVT.NEWS_GENRE_TECHNICS, self:GetAbonnementLevel(abonnementCount, 4))
 	MY.SetNewsAbonnement(TVT.NEWS_GENRE_CURRENTS, self:GetAbonnementLevel(abonnementCount, 5))
-	
+
 	--self.Task.CurrentBudget = self.Task.CurrentBudget - (abonnementCount * 10000)
 
 	self.Status = JOB_STATUS_DONE
@@ -88,31 +88,31 @@ function JobNewsAgency:Prepare(pParams)
 end
 
 function JobNewsAgency:Tick()
-	--TODO: EInfache Lösung	
+	--TODO: EInfache Lösung
 	local price = 0
-	
-	if (table.count(self.Newslist) > 0) then		
-		price = self.Newslist[1].news.ComputePrice()
+
+	if (table.count(self.Newslist) > 0) then
+		price = self.Newslist[1].newsEvent.ComputePrice()
 		if (self.Task.CurrentBudget >= price) then
-			debugMsg("Kaufe Nachricht: " .. self.Newslist[1].news.title .. " (" .. self.Newslist[1].id .. ") - Slot: 1 - Preis: " .. price)
+			debugMsg("Kaufe Nachricht: " .. self.Newslist[1].GetTitle() .. " (" .. self.Newslist[1].id .. ") - Slot: 1 - Preis: " .. price)
 			TVT.ne_doNewsInPlan(0)
 			TVT.ne_doNewsInPlan(0, self.Newslist[1].id)
 			self.Task.CurrentBudget = self.Task.CurrentBudget - price
 		end
 	end
 	if (table.count(self.Newslist) > 1) then
-		price = self.Newslist[2].news.ComputePrice()
-		if (self.Task.CurrentBudget >= price) then	
-			debugMsg("Kaufe Nachricht: " .. self.Newslist[2].news.title .. " (" .. self.Newslist[2].id .. ") - Slot: 2 - Preis: " .. price)
+		price = self.Newslist[2].newsEvent.ComputePrice()
+		if (self.Task.CurrentBudget >= price) then
+			debugMsg("Kaufe Nachricht: " .. self.Newslist[2].GetTitle() .. " (" .. self.Newslist[2].id .. ") - Slot: 2 - Preis: " .. price)
 			TVT.ne_doNewsInPlan(1)
 			TVT.ne_doNewsInPlan(1, self.Newslist[2].id)
 			self.Task.CurrentBudget = self.Task.CurrentBudget - price
 		end
 	end
 	if (table.count(self.Newslist) > 2) then
-		price = self.Newslist[3].news.ComputePrice()
-		if (self.Task.CurrentBudget >= price) then	
-			debugMsg("Kaufe Nachricht: " .. self.Newslist[3].news.title .. " (" .. self.Newslist[3].id .. ") - Slot: 3 - Preis: " .. price)
+		price = self.Newslist[3].newsEvent.ComputePrice()
+		if (self.Task.CurrentBudget >= price) then
+			debugMsg("Kaufe Nachricht: " .. self.Newslist[3].GetTitle() .. " (" .. self.Newslist[3].id .. ") - Slot: 3 - Preis: " .. price)
 			TVT.ne_doNewsInPlan(2)
 			TVT.ne_doNewsInPlan(2, self.Newslist[3].id)
 			self.Task.CurrentBudget = self.Task.CurrentBudget - price
@@ -122,20 +122,20 @@ function JobNewsAgency:Tick()
 end
 
 function JobNewsAgency:GetNewsList()
-	local currentNewsList = {}	
-	
-	for i = 0, MY.ProgrammePlan.GetNewsCount() - 1 do
-		local news = MY.ProgrammePlan.GetNewsFromList(i)
+	local currentNewsList = {}
+
+	for i = 0, MY.ProgrammeCollection.GetNewsCount() - 1 do
+		local news = MY.ProgrammeCollection.GetNewsAtIndex(i)
 		if (news.IsReadyToPublish() == 1) then
 			table.insert(currentNewsList, news)
 		end
 	end
-	
+
 	local sortMethod = function(a, b)
-		return a.news.GetAttractiveness() > b.news.GetAttractiveness()
+		return a.newsEvent.GetAttractiveness() > b.newsEvent.GetAttractiveness()
 	end
-	table.sort(currentNewsList, sortMethod)	
-	
+	table.sort(currentNewsList, sortMethod)
+
 	return currentNewsList
 end
 -- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
