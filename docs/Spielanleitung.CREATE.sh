18584e7857c4d35299cdc7ac80108b1c58e27c9c
roman4e <roman4e@gmail.com>
2025-02-16 23:29:34 +0200

    added ukrainian language localization

--- docs/Spielanleitung.CREATE.sh
+++ docs/Spielanleitung.CREATE.sh
@@ -1,3 +1,4 @@
 fmt -s -w 72 -g 71 manual_de.md > manual_de.txt
 fmt -s -w 72 -g 71 manual_en.md > manual_en.txt
 fmt -s -w 72 -g 71 manual_pl.md > manual_pl.txt
+fmt -s -w 72 -g 71 manual_ua.md > manual_ua.txt
